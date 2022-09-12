ff95963bb5bf70ec3a697a0b359a7bd7078358bf
Momshad Dinury <dinurymomshad.dev@gmail.com>
2022-09-12 15:57:31 +0600

    chore: building input field from scratch (wip)

---
+++ example/lib/input_outline_border.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+class InputOutlineBorder extends InputBorder {
+  const InputOutlineBorder({
+    super.borderSide = const BorderSide(),
+    this.borderRadius = const BorderRadius.all(Radius.circular(4.0)),
+  });
+
+  static bool _cornersAreCircular(BorderRadius borderRadius) {
+    return borderRadius.topLeft.x == borderRadius.topLeft.y &&
+        borderRadius.bottomLeft.x == borderRadius.bottomLeft.y &&
+        borderRadius.topRight.x == borderRadius.topRight.y &&
+        borderRadius.bottomRight.x == borderRadius.bottomRight.y;
+  }
+
+  /// The radii of the border's rounded rectangle corners.
+  ///
+  /// The corner radii must be circular, i.e. their [Radius.x] and [Radius.y]
+  /// values must be the same.
+  final BorderRadius borderRadius;
+
+  @override
+  bool get isOutline => true;
+
+  @override
+  InputOutlineBorder copyWith({
+    BorderSide? borderSide,
+    BorderRadius? borderRadius,
+  }) {
+    return InputOutlineBorder(
+      borderSide: borderSide ?? this.borderSide,
+      borderRadius: borderRadius ?? this.borderRadius,
+    );
+  }
+
+  @override
+  EdgeInsetsGeometry get dimensions {
+    return EdgeInsets.all(borderSide.width);
+  }
+
+  @override
+  InputOutlineBorder scale(double t) {
+    return InputOutlineBorder(
+      borderSide: borderSide.scale(t),
+      borderRadius: borderRadius * t,
+    );
+  }
+
+  @override
+  ShapeBorder? lerpFrom(ShapeBorder? a, double t) {
+    if (a is InputOutlineBorder) {
+      final InputOutlineBorder outline = a;
+      return InputOutlineBorder(
+        borderRadius: BorderRadius.lerp(outline.borderRadius, borderRadius, t)!,
+        borderSide: BorderSide.lerp(outline.borderSide, borderSide, t),
+      );
+    }
+    return super.lerpFrom(a, t);
+  }
+
+  @override
+  ShapeBorder? lerpTo(ShapeBorder? b, double t) {
+    if (b is InputOutlineBorder) {
+      final InputOutlineBorder outline = b;
+      return InputOutlineBorder(
+        borderRadius: BorderRadius.lerp(borderRadius, outline.borderRadius, t)!,
+        borderSide: BorderSide.lerp(borderSide, outline.borderSide, t),
+      );
+    }
+    return super.lerpTo(b, t);
+  }
+
+  @override
+  Path getInnerPath(Rect rect, {TextDirection? textDirection}) {
+    return Path()
+      ..addRRect(borderRadius
+          .resolve(textDirection)
+          .toRRect(rect)
+          .deflate(borderSide.width));
+  }
+
+  @override
+  Path getOuterPath(Rect rect, {TextDirection? textDirection}) {
+    return Path()..addRRect(borderRadius.resolve(textDirection).toRRect(rect));
+  }
+
+  @override
+  void paintInterior(Canvas canvas, Rect rect, Paint paint,
+      {TextDirection? textDirection}) {
+    canvas.drawRRect(borderRadius.resolve(textDirection).toRRect(rect), paint);
+  }
+
+  @override
+  bool get preferPaintInterior => true;
+
+  @override
+  void paint(
+    Canvas canvas,
+    Rect rect, {
+    double? gapStart,
+    double gapExtent = 0.0,
+    double gapPercentage = 0.0,
+    TextDirection? textDirection,
+  }) {
+    assert(_cornersAreCircular(borderRadius));
+
+    final Paint paint = borderSide.toPaint();
+    final RRect outer = borderRadius.toRRect(rect);
+    final RRect center = outer.deflate(borderSide.width / 2.0);
+
+    canvas.drawRRect(center, paint);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) {
+      return true;
+    }
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    return other is InputOutlineBorder &&
+        other.borderSide == borderSide &&
+        other.borderRadius == borderRadius;
+  }
+
+  @override
+  int get hashCode => Object.hash(borderSide, borderRadius);
+}
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,8 @@
+import 'package:example/input_field.dart';
+import 'package:example/input_outline_border.dart';
 import 'package:flutter/material.dart';
-import 'package:input_form_field/input_form_field.dart';
+
+import 'input_field_decorator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,99 +32,126 @@ class DemoScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(10.0),
           child: Column(
-            children: [
-              /// Standard - Outlined Border
-              InputFormField(
-                textEditingController: _controller,
-                labelText: "Standard - Outlined Border",
-                hintText: "Hint Text",
-                bottomMargin: 10, // Optional
+            children: const [
+              // Container(
+              //   // decoration: BoxDecoration(
+              //   //   border: Border.all(color: Colors.red)
+              //   // ),
+              //   child: TextFormField(
+              //     decoration: const InputDecoration(
+              //       hintText: "Hello",
+              //       labelText: "Label",
+              //       filled: true,
+              //       // contentPadding: EdgeInsets.all(10),
+              //       border: OutlineInputBorder(),
+              //       // border: InputBorder.none,
+              //     ),
+              //   ),
+              // ),
+              // SizedBox(height: 10),
+              InputField(
+                decoration: InputFieldDecoration(
+                    hintText: "Hello",
+                    labelText: "Label",
+                    filled: true,
+                    // contentPadding: EdgeInsets.all(10),
+                    border: InputOutlineBorder()
+                    // border: OutlineInputBorder(),
+                    // border: InputBorder.none,
+                    ),
               ),
-
-              /// No Border - Filled
-              InputFormField(
-                textEditingController: _controller,
-                labelText: "No Border - Filled",
-                hintText: "Hint Text",
-                fillColor: Colors.black12,
-                borderType: BorderType.none,
-                bottomMargin: 10, // Optional
-              ),
-
-              /// Default Password Field
-              InputFormField(
-                textEditingController: _controller,
-                labelText: "Default Password",
-                hintText: "*****",
-                password: EnabledPassword(),
-                bottomMargin: 10, // Optional
-              ),
-
-              /// Customize Default Password Field
-              InputFormField(
-                textEditingController: _controller,
-                labelText: "Custom Default Password",
-                hintText: "*****",
-                obscuringCharacter: "*",
-                password: EnabledPassword(
-                  showPasswordIcon: const Icon(
-                    Icons.add,
-                    color: Colors.amber,
-                  ),
-                  hidePasswordIcon: const Icon(
-                    Icons.visibility_off_sharp,
-                    color: Colors.amber,
-                  ),
-                ),
-                bottomMargin: 10, // Optional
-              ),
-
-              /// Custom
-              InputFormField(
-                textEditingController: _controller,
-                labelText: "Custom",
-                labelTextStyle: const TextStyle(fontSize: 25),
-                hintText: "Hint Text",
-                hintTextStyle: const TextStyle(fontSize: 18),
-                borderColor: Colors.deepOrange,
-                borderRadius: BorderRadius.circular(10),
-                bottomMargin: 10, // Optional
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-              ),
-
-              /// Custom 2
-              InputFormField(
-                textEditingController: _controller,
-                labelText: "Custom 2",
-                labelTextStyle: const TextStyle(color: Colors.deepOrange),
-                hintText: "Hint Text",
-                hintTextStyle: const TextStyle(color: Colors.deepOrangeAccent),
-                borderType: BorderType.outlined,
-                borderColor: Colors.deepOrange,
-                bottomMargin: 10, // Optional
-                borderRadius: BorderRadius.circular(10),
-              ),
-
-              /// Custom 3 (Suffix)
-              InputFormField(
-                textEditingController: _controller,
-                prefix: const Icon(Icons.mail),
-                labelText: "Email",
-                hintText: "abc@email.com",
-                borderType: BorderType.outlined,
-                bottomMargin: 10, // Optional
-              ),
-
-              /// Custom 4 (Suffix, Prefix)
-              InputFormField(
-                textEditingController: _controller,
-                prefix: const Icon(Icons.mail),
-                labelText: "Email",
-                hintText: "abc@email.com",
-                suffix: const Icon(Icons.info),
-                borderType: BorderType.outlined,
-                bottomMargin: 10, // Optional
-              ),
+              // /// Standard - Outlined Border
+              // InputFormField(
+              //   textEditingController: _controller,
+              //   labelText: "Standard - Outlined Border",
+              //   hintText: "Hint Text",
+              //   bottomMargin: 10, // Optional
+              // ),
+              //
+              // /// No Border - Filled
+              // InputFormField(
+              //   textEditingController: _controller,
+              //   labelText: "No Border - Filled",
+              //   hintText: "Hint Text",
+              //   fillColor: Colors.black12,
+              //   borderType: BorderType.none,
+              //   bottomMargin: 10, // Optional
+              // ),
+              //
+              // /// Default Password Field
+              // InputFormField(
+              //   textEditingController: _controller,
+              //   labelText: "Default Password",
+              //   hintText: "*****",
+              //   password: EnabledPassword(),
+              //   bottomMargin: 10, // Optional
+              // ),
+              //
+              // /// Customize Default Password Field
+              // InputFormField(
+              //   textEditingController: _controller,
+              //   labelText: "Custom Default Password",
+              //   hintText: "*****",
+              //   obscuringCharacter: "*",
+              //   password: EnabledPassword(
+              //     showPasswordIcon: const Icon(
+              //       Icons.add,
+              //       color: Colors.amber,
+              //     ),
+              //     hidePasswordIcon: const Icon(
+              //       Icons.visibility_off_sharp,
+              //       color: Colors.amber,
+              //     ),
+              //   ),
+              //   bottomMargin: 10, // Optional
+              // ),
+              //
+              // /// Custom
+              // InputFormField(
+              //   textEditingController: _controller,
+              //   labelText: "Custom",
+              //   labelTextStyle: const TextStyle(fontSize: 25),
+              //   hintText: "Hint Text",
+              //   hintTextStyle: const TextStyle(fontSize: 18),
+              //   borderColor: Colors.deepOrange,
+              //   borderRadius: BorderRadius.circular(10),
+              //   bottomMargin: 10, // Optional
+              //   floatingLabelBehavior: FloatingLabelBehavior.always,
+              // ),
+              //
+              // /// Custom 2
+              // InputFormField(
+              //   textEditingController: _controller,
+              //   labelText: "Custom 2",
+              //   labelTextStyle: const TextStyle(color: Colors.deepOrange),
+              //   hintText: "Hint Text",
+              //   hintTextStyle: const TextStyle(color: Colors.deepOrangeAccent),
+              //   borderType: BorderType.outlined,
+              //   borderColor: Colors.deepOrange,
+              //   bottomMargin: 10, // Optional
+              //   borderRadius: BorderRadius.circular(10),
+              // ),
+              //
+              // /// Custom 3 (Suffix)
+              // InputFormField(
+              //   textEditingController: _controller,
+              //   prefix: const Icon(Icons.mail),
+              //   labelText: "Email",
+              //   hintText: "abc@email.com",
+              //   borderType: BorderType.outlined,
+              //   bottomMargin: 10, // Optional
+              // ),
+              //
+              // /// Custom 4 (Suffix, Prefix)
+              // InputFormField(
+              //   textEditingController: _controller,
+              //   prefix: const Icon(Icons.mail),
+              //   labelText: "Email",
+              //   hintText: "abc@email.com",
+              //   suffix: const Icon(Icons.info),
+              //   borderType: BorderType.outlined,
+              //   bottomMargin: 10, // Optional
+              // ),
             ],
           ),
         ),
